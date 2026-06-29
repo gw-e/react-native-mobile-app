07b86baf37aaaa5b77877b6d8a2640275869e2be
gw-e <gabrielwaadeeriksen@gmail.com>
2026-06-29 10:36:41 +0200

    Add nativewind to project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
+import "@/global.css";
+import { Link } from 'expo-router';
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Link href="/test" className="mt-4 rounded bg-black text-white p-4">
+        Go to Test Page
+      </Link>
     </View>
   );
 }
